67444c0e6389440cbbc6ad5ed98356e06828f2cf
YinHao <shdic2@163.com>
2024-05-31 13:40:02 +0800

    add

--- docs/lottery/script.js
+++ docs/lottery/script.js
@@ -3,6 +3,7 @@ const exclude = document.querySelector("#exclude");
 const random10 = document.querySelector("#random10");
 const output = document.querySelector("#output");
 const numDom = document.querySelector("#num");
+const create = document.querySelector("#create");
 
 let lock = false,
   num = 0;
@@ -20,6 +21,62 @@ exclude.onclick = () => {
     excludeNumbers();
   }
 };
+
+create.onclick = () => {
+  const outputVal = output.value;
+  console.log("outputVal", outputVal);
+  const outputValArr = outputVal.split("\n");
+  console.log("outputValArr", outputValArr);
+  createCanvas(outputValArr);
+};
+
+const createCanvas = (arr) => {
+  var canvas = document.createElement("canvas");
+  canvas.width = 400;
+  canvas.height = 200;
+  // document.body.appendChild(canvas);
+  var ctx = canvas.getContext("2d");
+  var values = arr;
+  var rows = values.length;
+  var cols = values[0].length;
+  var cellWidth = canvas.width / cols;
+  var cellHeight = canvas.height / rows;
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      var value = values[i][j];
+      var x = j * cellWidth;
+      var y = i * cellHeight;
+      if (j === cols - 1) {
+        ctx.fillStyle = "#4477CE";
+      } else {
+        ctx.fillStyle = "#DB6B97";
+      }
+      ctx.fillRect(x, y, cellWidth, cellHeight);
+      ctx.fillStyle = "#ffffff";
+      ctx.font = "18px hack";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(value.toString(), x + cellWidth / 2, y + cellHeight / 2);
+    }
+  }
+  var dataURL = canvas.toDataURL("image/png");
+  var link = document.createElement("a");
+  link.href = dataURL;
+  link.download = "canvas_image.png";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+function convertArray(arr) {
+  var result = "";
+  for (var i = 0; i < arr.length; i++) {
+    var row = arr[i];
+    var rowString = row.slice(0, row.length - 1).join(", ");
+    result += rowString + " - " + row[row.length - 1] + "\n";
+  }
+  return result.trim();
+}
 
 function generateLottoNumbers(numberOfTickets) {
   let lottoNumbers = [];
